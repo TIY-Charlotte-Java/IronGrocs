584dda7ed87b3f855d5ef93211af0983baf01cdb
Ben Sterrett <raynjamin@gmail.com>
2017-04-19 19:07:53 -0400

    Fixing port

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -5,6 +5,12 @@ import spark.Spark;
 public class Main {
 
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+
+        if (port != null) {
+            Spark.port(Integer.valueOf(port));
+        }
+
         // https://iron-grocs.herokuapp.com/grocs
         Spark.webSocket("/grocs", GrocWebSocket.class);
         Spark.init();
