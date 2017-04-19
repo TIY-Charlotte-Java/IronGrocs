574bc36c80f1450e8f008063d680268584c8a1bc
Ben Sterrett <raynjamin@gmail.com>
2017-04-19 19:04:29 -0400

    fixing order of initialization

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -5,11 +5,12 @@ import spark.Spark;
 public class Main {
 
     public static void main(String[] args) {
+        // https://iron-grocs.herokuapp.com/grocs
+        Spark.webSocket("/grocs", GrocWebSocket.class);
+        Spark.init();
+
         Spark.after(((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
         }));
-
-        Spark.webSocket("/grocs", GrocWebSocket.class);
-        Spark.init();
     }
 }
