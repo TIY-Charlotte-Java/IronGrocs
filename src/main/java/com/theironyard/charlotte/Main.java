8af85ef4678734d3c7c115bba1832d74098d115e
Ben Sterrett <raynjamin@gmail.com>
2017-04-19 18:58:07 -0400

    Adding cors

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -5,6 +5,10 @@ import spark.Spark;
 public class Main {
 
     public static void main(String[] args) {
+        Spark.after(((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+        }));
+
         Spark.webSocket("/grocs", GrocWebSocket.class);
         Spark.init();
     }
